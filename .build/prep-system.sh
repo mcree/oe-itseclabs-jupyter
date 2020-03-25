cb789a60094820da8986747344f325e7e64a458b
Rigó Ernő <rigo.erno@sztaki.mta.hu>
2020-03-25 15:59:31 +0100

    Removed misbehaving dependencies

--- .build/prep-system.sh
+++ .build/prep-system.sh
@@ -5,13 +5,13 @@
 cd "$(dirname $0)"
 [[ -f ".flag-prep-system-done" ]] && exit 0
 set -e +x
-pipenv lock
+#pipenv lock
 pipenv install --system
-jupyter contrib nbextension install
-jupyter serverextension enable --py jupyterlab_git
+#jupyter contrib nbextension install
+#jupyter serverextension enable --py jupyterlab_git
 jupyter nbextensions_configurator enable
-jupyter labextension install --no-build @jupyter-widgets/jupyterlab-manager
-jupyter labextension install --no-build @jupyterlab/git
-jupyter lab build
+#jupyter labextension install --no-build @jupyter-widgets/jupyterlab-manager
+#jupyter labextension install --no-build @jupyterlab/git
+#jupyter lab build
 touch .flag-prep-system-done
 exit 0
