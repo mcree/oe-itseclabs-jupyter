d79f8b7266e25bfe66b9bc06975f62fe135601b4
Rigó Ernő <rigo.erno@sztaki.mta.hu>
2020-04-01 12:53:20 +0200

    downgraded jupyter lab to 1.2.x

--- .build/prep-system.sh
+++ .build/prep-system.sh
@@ -9,11 +9,12 @@ set -e +x
 #pipenv install --system
 #pipenv run pip3 freeze > .build/requirements.txt
 #jupyter contrib nbextension install
-#jupyter serverextension enable --py jupyterlab_git
+remote_ikernel manage --add --host=itseclabs --kernel_cmd="sudo python3 -m zsh_jupyter_kernel -f {connection_file}" --name="ZSH" --system --language=zsh --interface=ssh
+jupyter serverextension enable --py jupyterlab_git
 python3 -m sshkernel install
 jupyter nbextensions_configurator enable
-jupyter labextension install --no-build @jupyter-widgets/jupyterlab-manager@2.0
-#jupyter labextension install --no-build @jupyterlab/git
+jupyter labextension install --no-build @jupyter-widgets/jupyterlab-manager@1.1
+jupyter labextension install --no-build @jupyterlab/git
 jupyter lab build
 touch .flag-prep-system-done
 exit 0
