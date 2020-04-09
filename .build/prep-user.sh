8b81cbd3292d1f15222013f82d9ddbf7fc9d1b83
Rigó Ernő <rigo.erno@sztaki.mta.hu>
2020-04-09 15:50:10 +0200

    add ssh workspace

--- .build/prep-user.sh
+++ .build/prep-user.sh
@@ -8,5 +8,6 @@ set -e +x
 dotnet tool install -g dotnet-script
 dotnet tool install -g --add-source "https://dotnet.myget.org/F/dotnet-try/api/v3/index.json" Microsoft.dotnet-interactive
 dotnet interactive jupyter install
+jupyter lab workspaces import ../workspaces/ssh.json
 touch .flag-prep-user-done
 exit 0
